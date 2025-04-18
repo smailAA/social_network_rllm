df6add878d44b399ef0d7ee1918e2bf55aae705c
smailAA <Zyuxin719@sjtu.edu.cn>
2025-04-18 17:01:39 +0800

    update tlf2k

--- musician_classification/test_api.py
+++ musician_classification/test_api.py
@@ -1,7 +1,7 @@
 from openai import OpenAI
 
 
-client = OpenAI(api_key="sk-m6iili3XRbC5jCd0360f9f96A4E94fC0B71257Fe8a568cF5", base_url="http://111.186.56.172:3000/v1")
+client = OpenAI(api_key="your_api_key", base_url="http://111.186.56.172:3000/v1")
 
 response = client.chat.completions.create(
     model="deepseek-chat",
